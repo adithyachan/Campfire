8867fe067278e021d4f22a06f64a4106915297d8
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2024-02-01 15:56:56 -0500

    Initial Commit

---
+++ Campfire/CampfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampfireApp.swift
+//  Campfire
+//
+//  Created by Adithya Chandrasekar on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CampfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
