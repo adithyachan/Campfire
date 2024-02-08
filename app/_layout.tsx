9f18e4da4980329c5fdcc0ba99040abac2f27e1c
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2024-02-07 21:16:40 -0500

    login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,18 @@
 
 import '../global.css';
-
-
-
-
-	import { Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 
 export const unstable_settings = {
 	// Ensure that reloading on `/modal` keeps a back button present.
-	initialRouteName: "(tabs)",
+	initialRouteName: "auth",
 };
 
 export default function RootLayout() {
-  	
-
   	return (
-    	
-		<Stack>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-			<Stack.Screen name="modal" options={{ presentation: "modal" }} />
-		</Stack>
-		
+			<Stack>
+				<Stack.Screen name='auth' />
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+			</Stack>
   	);
 }
