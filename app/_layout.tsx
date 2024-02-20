8291e89a4f6ce10f2296a036b48420364ec8c454
Param Patel <51967918+parampatel21@users.noreply.github.com>
2024-02-20 15:53:22 -0500

    resetting the auth state bc of testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,14 +5,14 @@ import { config } from "@gluestack-ui/config"
 
 export const unstable_settings = {
 	// Ensure that reloading on `/modal` keeps a back button present.
-	initialRouteName: "(tabs)",
+	initialRouteName: "auth",
 };
 
 export default function RootLayout() {
   	return (
 			<GluestackUIProvider config={config}>
 				<Stack>
-					{/* <Stack.Screen name='auth' options={{ headerShown: false }}/> */}
+					<Stack.Screen name='auth' options={{ headerShown: false }}/>
 					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 				</Stack>
 			</GluestackUIProvider>
