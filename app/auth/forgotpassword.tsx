4f541e38382a07d5c81003ab4e3d4de569ef7334
albearx <albert.xu.13103@icloud.com>
2024-02-22 21:20:25 -0500

    Create forgotpassword.tsx

---
+++ app/auth/forgotpassword.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { AddIcon, AlertCircleIcon, Button, ButtonIcon, ButtonText, FormControl, FormControlError, FormControlErrorIcon, FormControlErrorText, FormControlHelper, FormControlHelperText, FormControlLabel, FormControlLabelText, HStack, Input, InputField, Text, VStack } from '@gluestack-ui/themed';
+import { supabase } from 'utils/supabase';
+import { router } from 'expo-router';
+
+const ForgotPassword = () => {
+
+}
+
+export default ForgotPassword
