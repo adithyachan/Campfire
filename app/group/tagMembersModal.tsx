281c91c48e632bc2b1dfee995e9332e18afa7fcd
Param Patel <51967918+parampatel21@users.noreply.github.com>
2024-04-25 00:18:17 -0400

    let users know they cant post without anyone else

--- app/group/tagMembersModal.tsx
+++ app/group/tagMembersModal.tsx
@@ -41,7 +41,7 @@ const TagMembersModal = ({
   groupMembers,
   onTagsConfirmed,
   initialSelectedMembers,
-  groupId
+  groupId,
 }: TagMembersModalProps) => {
   const [selectedMembers, setSelectedMembers] = useState<string[]>([]);
 
@@ -63,30 +63,33 @@ const TagMembersModal = ({
     return selectedMembers.includes(userId);
   };
 
-  const handleConfirmTags = async() => {
+  const handleConfirmTags = async () => {
     onTagsConfirmed(selectedMembers); // Pass selected members back to the parent component
 
-    const {data: {user}} = await supabase.auth.getUser();
-    const current_user_id = user?.id
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    const current_user_id = user?.id;
 
-    const {data: groupData} = await supabase
+    const { data: groupData } = await supabase
       .from('groups')
       .select('name')
       .eq('group_id', groupId)
-      .single()
-    
-    const groupName = groupData?.name
+      .single();
 
+    const groupName = groupData?.name;
 
     selectedMembers.forEach(async (tagged_user_id) => {
       if (tagged_user_id !== current_user_id) {
-        const { error } = await supabase
-          .from('notifications')
-          .insert({user_id: tagged_user_id, title: 'You\'ve been tagged', body: `Someone in group ${groupName} has tagged you in a post!`, event: 'Tagged', redirect_to: groupId})
+        const { error } = await supabase.from('notifications').insert({
+          user_id: tagged_user_id,
+          title: "You've been tagged",
+          body: `Someone in group ${groupName} has tagged you in a post!`,
+          event: 'Tagged',
+          redirect_to: groupId,
+        });
       }
-      
-    })
-    
+    });
 
     onClose(); // Close the modal
   };
@@ -102,25 +105,29 @@ const TagMembersModal = ({
           </ModalCloseButton>
         </ModalHeader>
         <ModalBody>
-          {groupMembers.map((member) => (
-            <Box
-              key={member.user_id}
-              flexDirection="row"
-              justifyContent="space-between"
-              alignItems="center"
-              mb="$4">
-              <Box flexDirection="row" alignItems="center">
-                <Text>
-                  {member.first_name} {member.last_name}
-                </Text>
+          {groupMembers.length > 0 ? (
+            groupMembers.map((member) => (
+              <Box
+                key={member.user_id}
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+                mb="$4">
+                <Box flexDirection="row" alignItems="center">
+                  <Text>
+                    {member.first_name} {member.last_name}
+                  </Text>
+                </Box>
+                <Button
+                  action={isTagged(member.user_id) ? 'negative' : 'positive'}
+                  onPress={() => toggleTagMember(member.user_id)}>
+                  <ButtonText>{isTagged(member.user_id) ? 'Untag' : 'Tag'}</ButtonText>
+                </Button>
               </Box>
-              <Button
-                action={isTagged(member.user_id) ? 'negative' : 'positive'}
-                onPress={() => toggleTagMember(member.user_id)}>
-                <ButtonText>{isTagged(member.user_id) ? 'Untag' : 'Tag'}</ButtonText>
-              </Button>
-            </Box>
-          ))}
+            ))
+          ) : (
+            <Text>No members available to tag. Invite friends to join your group!</Text>
+          )}
         </ModalBody>
         <ModalFooter>
           <Button onPress={handleConfirmTags}>
